d62dd448091416bad1cc6441794172ec8fa0a71a
Julie Kovalaske <juliekovalaske@gmail.com>
2020-01-01 16:16:26 +0000

    function app version

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import List from './components/List';
 import Note from './components/Note';
 
 function App() {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+  
+  const { showNote } = this.state;
+
   return (
     <div className="App">
       <Nav />
-      <List />
-      <Note />
+      { showNote ? <Note/> : <List /> }
     </div>
   );
 }
+  
+
 
 export default App;
