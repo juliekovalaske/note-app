b257255117ea2ebf734d401cc43cc6da0b2a76b0
Julie Kovalaske <juliekovalaske@gmail.com>
2020-01-03 01:13:19 +0000

    axios

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 class List extends React.Component {
+
+  componentWillMount() {
+    this.props.getNotes();
+  }
+
+
   render() {
     return (
-      <div ClassName="list-container">
+      <div className="list-container">
         List Component
       </div>
     )
