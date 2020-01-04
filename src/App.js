439731bd99d6e8361a4a6ed25aa685d9d6c2ff6a
Julie Kovalaske <juliekovalaske@gmail.com>
2020-01-04 16:02:01 +0000

    delete button functionality

--- src/App.js
+++ src/App.js
@@ -50,6 +50,13 @@ class App extends Component {
     .catch((err) => console.log(err.response.data) );
   }
 
+  deleteNote = (id) => {
+    const newNoteState = this.state.notes.filter((note) => note.id !== id );
+    axios.delete(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ notes: newNoteState }))
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render () {
     const { showNote, notes, note } = this.state;
 
@@ -66,6 +73,7 @@ class App extends Component {
             getNotes={this.getNotes}
             notes={notes}
             getNote={this.getNote}
+            deleteNote={this.deleteNote}
           /> 
         }
       </div>
